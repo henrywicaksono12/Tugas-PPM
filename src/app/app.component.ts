b30e2f2dea376328f6baea3962597b9b0149e41a
henrywicaksono12 <c14180014@john.petra.ac.id>
2020-04-15 09:59:50 +0700

    Commit 1 - Pola Bilangan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+
   name = 'Angular';
+
+  a = null;
+  b = null;
+  hasil = 0;
+  pola = [];
+
+  start(a, b){
+    let c = Math.pow(a, b);
+
+    this.pola = [];
+    this.hasil = c;
+    let row = "";
+
+    if(this.hasil % 2 == 0)
+    {
+      for (let i = b; i <= c; i++)
+      {
+        row = "";
+
+        for(let j = 0; j < i; j++)
+        {
+          row += "*";
+        }
+
+        this.pola.push(row);
+      }
+    }
+    else
+    {
+      if(a >= b)
+      {
+        let d = a;
+      }
+      else{
+        let d = b;
+      }
+
+      for (let i = 0; i < d; i++)
+      {
+        row = "";
+
+        if (i < b)
+        {
+          for(let j = 0; j < b-i; j++)
+          {
+            row += "*";
+          }
+        }
+
+        if(i < a)
+        {
+          for(let j = 0; j <= i; j++)
+          {
+            row += "o";
+          }
+        }
+
+        this.pola.push(row);
+      }
+    }
+  }
+
+  clear(a, b){
+    this.a = null;
+    this.b = null;
+    this.hasil = 0;
+    this.pola = [];
+  }
 }
